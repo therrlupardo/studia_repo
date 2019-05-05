9fe63dd0a0b4e4680bb35697fa403cff0391b6f5
Mateusz Buchajewicz <theherrlupardo@gmail.com>
2019-05-05 19:30:36 +0200

    fix formatting

--- Semestr4/Przetwarzanie Rozproszone/Laboratoria/Lab7/source.c
+++ Semestr4/Przetwarzanie Rozproszone/Laboratoria/Lab7/source.c
@@ -9,103 +9,116 @@ int *primes;
 int counter = 0;
 pthread_mutex_t mutex;
 
-void *prime(int _n) {
-    bool isPrime = true;
-    
-    if (_n == 0 || _n == 1) {
-        return 0;
+void *prime(int _n)
+{
+	bool isPrime = true;
+
+	if (_n == 0 || _n == 1)
+	{
+		return 0;
 	}
-    
-    for (int __i = 2; __i * __i <= _n; __i++) {
-        if (_n % __i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
-    
-    if (isPrime == true) {
-        pthread_mutex_lock(&mutex);
-    	primes[counter] = _n;
+
+	for (int __i = 2; __i * __i <= _n; __i++)
+	{
+		if (_n % __i == 0)
+		{
+			isPrime = false;
+			break;
+		}
+	}
+
+	if (isPrime == true)
+	{
+		pthread_mutex_lock(&mutex);
+		primes[counter] = _n;
 		counter++;
-	    pthread_mutex_unlock(&mutex);
-    }
-    return 0;
+		pthread_mutex_unlock(&mutex);
+	}
+	return 0;
 }
 
-void bubbleSort(int* array, const int length) {
-	for ( int __i = 0; __i < length; __i++){
-		for(int __j = __i + 1; __j < length; __j++) {
-			if (array[__i] > array[__j]){
+void bubbleSort(int *array, const int length)
+{
+	for (int __i = 0; __i < length; __i++)
+	{
+		for (int __j = __i + 1; __j < length; __j++)
+		{
+			if (array[__i] > array[__j])
+			{
 				int tmp = array[__j];
 				array[__j] = array[__i];
 				array[__i] = tmp;
 			}
 		}
-	}	
+	}
 }
 
 int main()
 {
-    int min;
-    int max;
-    int threadsCreated = 0;
-    pthread_mutex_init(&mutex, NULL);
-    pthread_t threads[THREADS];
+	int min;
+	int max;
+	int threadsCreated = 0;
+	pthread_mutex_init(&mutex, NULL);
+	pthread_t threads[THREADS];
 
 	printf("Podaj poczatek zakresu:\n");
-    scanf("%d", &min);
+	scanf("%d", &min);
 	printf("Podaj koniec zakresu:\n");
-    scanf("%d", &max);
-    
-	if (min > max || min < 0 || max < 0) {
+	scanf("%d", &max);
+
+	if (min > max || min < 0 || max < 0)
+	{
 		printf("Invalid input. Rerun and try again!\n");
 		return 1;
 	}
 
-    primes = malloc(sizeof(int)*(max-min));
-    
-    for (int __i = min; __i <= max; __i++)
-    {
- 
-        int tmp = (__i - min) % THREADS;
-        
-        pthread_create(&threads[tmp], NULL , prime, __i);
-        threadsCreated++;
+	primes = malloc(sizeof(int) * (max - min));
 
-        if (threadsCreated == THREADS) {
-            for (int __j = 0; __j < THREADS; __j++)
-            {
-                pthread_join(threads[__j], NULL);
-            }
-            threadsCreated = 0;
-        }
-        
-    }
+	for (int __i = min; __i <= max; __i++)
+	{
 
-	if (threadsCreated != 0) {
-		for (int __j = 0; __j < threadsCreated; __j++)
-            {
-                pthread_join(threads[__j], NULL);
-            }
+		int tmp = (__i - min) % THREADS;
+
+		pthread_create(&threads[tmp], NULL, prime, __i);
+		threadsCreated++;
+
+		if (threadsCreated == THREADS)
+		{
+			for (int __j = 0; __j < THREADS; __j++)
+			{
+				pthread_join(threads[__j], NULL);
+			}
+			threadsCreated = 0;
+		}
 	}
 
-	if (counter != 0) {
+	if (threadsCreated != 0)
+	{
+		for (int __j = 0; __j < threadsCreated; __j++)
+		{
+			pthread_join(threads[__j], NULL);
+		}
+	}
+
+	if (counter != 0)
+	{
 		printf("Primes:\n");
 		for (int __i = 0; __i < counter; __i++)
 		{
-		     printf("%d ", primes[__i]);
+			printf("%d ", primes[__i]);
 		}
 		printf("\n");
 	}
 
-    printf("Found %d prime number(s) in range(%d, %d).\n", counter, min, max);
+	printf("Found %d prime number(s) in range(%d, %d).\n", counter, min, max);
 
-	if(counter != 0) {
+	if (counter != 0)
+	{
 		bubbleSort(primes, counter);
 		printf("Primes sorted:\n");
 		for (int __i = 0; __i < counter; __i++)
 		{
-		     printf("%d ", primes[__i]);
+			printf("%d ", primes[__i]);
 		}
 		printf("\n");
 	}
